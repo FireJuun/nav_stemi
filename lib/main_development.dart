6a147310cad6a3d272c103abd05eedafc38af757
John Manning <john.d.manning@gmail.com>
2025-02-07 10:42:34 -0500

    :sparkles: add ability to push CSV data to update hospital/ED list

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nav_stemi/firebase_options_dev.dart';
 import 'package:nav_stemi/nav_stemi.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Animate.restartOnHotReload = true;
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
