04c7a490d84f38e26b23abfef152106058395299
John Manning <john.d.manning@gmail.com>
2025-03-23 09:30:27 -0400

    :construction: adding/updating fhir extensions

--- lib/src/features/fhir_sync/domain/encounter.dart
+++ lib/src/features/fhir_sync/domain/encounter.dart
@@ -2,49 +2,6 @@ import 'package:fhir_r4/fhir_r4.dart';
 import 'package:nav_stemi/nav_stemi.dart';
 
 extension EncounterX on Encounter {
-  /// This tracks a single instance of STEMI care from an EMS perspective.
-  /// Timestamps and their NEMSIS data connections are provided
-
-  Encounter defaultEmsEncounter() {
-    /// These fields define an EMS Encounter, in case other types
-    /// are added in the future.
-    /// R4 Spec: https://hl7.org/fhir/R4/encounter.html
-    return Encounter(
-      status: EncounterStatus.in_progress,
-
-      /// R4 Spec: https://hl7.org/fhir/R4/v3/ActEncounterCode/vs.html
-      class_: Coding(
-        system: FhirUri('http://terminology.hl7.org/CodeSystem/v3-ActCode'),
-        code: FhirCode('FLD'),
-        display: FhirString('field'),
-      ),
-
-      /// R4 Spec: https://hl7.org/fhir/R4/v3/ActPriority/vs.html
-      priority: CodeableConcept(
-        coding: [
-          Coding(
-            system:
-                FhirUri('http://terminology.hl7.org/CodeSystem/v3-ActPriority'),
-            code: FhirCode('EM'),
-            display: FhirString('emergency'),
-          ),
-        ],
-      ),
-
-      /// R4 Spec: https://hl7.org/fhir/R4/valueset-service-type.html
-      serviceType: CodeableConcept(
-        coding: [
-          Coding(
-            system:
-                FhirUri('http://terminology.hl7.org/CodeSystem/service-type'),
-            code: FhirCode('226'),
-            display: FhirString('Ambulance'),
-          ),
-        ],
-      ),
-    );
-  }
-
   Encounter updateLocations(TimeMetricsModel timeMetrics) {
     final arrivedAtPatient = timeMetrics.timeArrivedAtPatient;
     final firstEkg = timeMetrics.timeOfFirstEkg();
@@ -95,5 +52,93 @@ extension EncounterX on Encounter {
         ),
     ];
     return copyWith(location: locations);
+  }
+
+  /// This tracks a single instance of STEMI care from an EMS perspective.
+  /// Timestamps and their NEMSIS data connections are provided
+
+  Encounter asEmsEncounter() {
+    /// These fields define an EMS Encounter, in case other types
+    /// are added in the future.
+    /// R4 Spec: https://hl7.org/fhir/R4/encounter.html
+    return Encounter(
+      status: EncounterStatus.in_progress,
+
+      /// R4 Spec: https://hl7.org/fhir/R4/v3/ActEncounterCode/vs.html
+      class_: Coding(
+        system: FhirUri('http://terminology.hl7.org/CodeSystem/v3-ActCode'),
+        code: FhirCode('FLD'),
+        display: FhirString('field'),
+      ),
+
+      /// R4 Spec: https://hl7.org/fhir/R4/v3/ActPriority/vs.html
+      priority: CodeableConcept(
+        coding: [
+          Coding(
+            system:
+                FhirUri('http://terminology.hl7.org/CodeSystem/v3-ActPriority'),
+            code: FhirCode('EM'),
+            display: FhirString('emergency'),
+          ),
+        ],
+      ),
+
+      /// R4 Spec: https://hl7.org/fhir/R4/valueset-service-type.html
+      serviceType: CodeableConcept(
+        coding: [
+          Coding(
+            system:
+                FhirUri('http://terminology.hl7.org/CodeSystem/service-type'),
+            code: FhirCode('226'),
+            display: FhirString('Ambulance'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Encounter updatePatientReference(Patient patient) {
+    return copyWith(subject: patient.thisReference);
+  }
+
+  Encounter updateCardiologistReference(Practitioner cardiologist) {
+    final participants = participant ?? [];
+
+    /// The cardiologist is always an indirect target.
+    final indirectTarget = [
+      CodeableConcept(
+        coding: [
+          /// R4 Spec: https://hl7.org/fhir/R4/v3/ParticipationType/cs.html#v3-ParticipationType-_ParticipationAncillary
+          Coding(
+            system: FhirUri(
+              'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
+            ),
+            code: FhirCode('IND'),
+            display: FhirString('indirect target'),
+          ),
+        ],
+      ),
+    ];
+
+    final cardiologistParticipant = EncounterParticipant(
+      individual: cardiologist.thisReference,
+      type: indirectTarget,
+    );
+
+    /// Find the index of the existing indirect target participant, if any
+    final indirectTargetIndex =
+        participants.indexWhere((p) => p.type == indirectTarget);
+
+    /// Update the old cardiologist reference, or
+    /// add a new one if it doesn't exist
+    (indirectTargetIndex != -1)
+        ? participants.replaceRange(
+            indirectTargetIndex,
+            indirectTargetIndex + 1,
+            [cardiologistParticipant],
+          )
+        : participants.add(cardiologistParticipant);
+
+    return copyWith(participant: participants);
   }
 }
