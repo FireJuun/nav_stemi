4b56a84605f416e72e4abda99081d092896bd145
Ryan Edge <ryan.c.edge@outlook.com>
2025-09-15 00:49:15 -0400

    Refactor AppUser class hierarchy to include uid in the base class and enhance FirebaseAppUser with displayName

--- lib/src/features/auth/domain/app_user.dart
+++ lib/src/features/auth/domain/app_user.dart
@@ -2,10 +2,24 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis_auth/googleapis_auth.dart' as auth show AuthClient;
 import 'package:http/http.dart' as http;
 
-sealed class AppUser {}
+sealed class AppUser {
+  const AppUser({required this.uid});
+
+  final String uid;
+}
+
+final class FirebaseAppUser extends AppUser {
+  const FirebaseAppUser({required super.uid, this.displayName});
+
+  final String? displayName;
+
+  @override
+  List<Object?> get props => [uid, displayName];
+}
 
 class GoogleAppUser extends AppUser {
-  GoogleAppUser({required this.user, required this.client});
+  GoogleAppUser({required this.user, required this.client})
+      : super(uid: user.id);
 
   final GoogleSignInAccount user;
   final auth.AuthClient client;
@@ -15,7 +29,7 @@ class ServiceAccountUser extends AppUser {
   ServiceAccountUser({
     required this.email,
     required this.client,
-  });
+  }) : super(uid: '');
 
   final String email;
   final http.Client client;
@@ -28,7 +42,7 @@ class ServiceAccountUser extends AppUser {
 class FakeAppUser extends AppUser {
   FakeAppUser({
     required this.email,
-  });
+  }) : super(uid: '');
 
   final String email;
 }
