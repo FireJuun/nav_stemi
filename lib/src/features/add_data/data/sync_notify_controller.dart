55b68bcdf79e1a6d477c669529ede14ad690b2fa
Ryan Edge <ryan@cancode.dev>
2025-10-03 11:06:03 -0500

    Enhances sync state and data synchronization logic
    Updates the `SyncNotifyController` to manage and synchronize patient and time metric data more effectively.
    
    - Populates `SyncNotifyState` with `PatientInfoModel` and `TimeMetricsModel` at initialization.
    - Transforms `autosyncPeers` in the state from device IDs (`List<String>`) to richer `PeerUserData` objects (`List<PeerUserData>`).
    - Refactors `onHandleSync` to automatically gather patient and time metric data for synchronization.
    - Introduces `Result` type for `onHandleSync` to provide robust error handling for sync operations.
    - Updates `addAutosyncPeer` and `removeAutosyncPeer` to reflect `PeerUserData` in the state.

--- lib/src/features/add_data/data/sync_notify_controller.dart
+++ lib/src/features/add_data/data/sync_notify_controller.dart
@@ -4,8 +4,8 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nav_stemi/src/data/services/bridgefy_service.dart';
 import 'package:nav_stemi/src/features/add_data/data/peer_user_repository.dart';
-import 'package:nav_stemi/src/features/add_data/domain/peer_user_data.dart';
 import 'package:nav_stemi/src/features/export.dart';
+import 'package:nav_stemi/src/utils/result.dart';
 
 class SyncNotifyController extends AsyncNotifier<SyncNotifyState> {
   static final provider =
@@ -15,69 +15,114 @@ class SyncNotifyController extends AsyncNotifier<SyncNotifyState> {
 
   @override
   FutureOr<SyncNotifyState> build() async {
-    ref.listen(BridgefyService.eventsProvider, (previous, next) {});
-    final connectedPeers = await ref.watch(
-      BridgefyService.connectedPeersProvider.future,
-    );
+    // ref.listen(BridgefyService.eventsProvider, (previous, next) {});
+    // final connectedPeers = await ref.watch(
+    //   BridgefyService.connectedPeersProvider.future,
+    // );
 
-    final autosyncPeers =
-        ref.watch(sharedPreferencesRepositoryProvider).getAutoSyncPeers();
+    // final autosyncPeers =
+    //     ref.watch(sharedPreferencesRepositoryProvider).getAutoSyncPeers();
 
-    final connectedPeersData = await ref
-        .watch(PeerInfoRepository.provider)
-        .fetchPeerUserData(connectedPeers);
+    // final connectedPeersData = await ref
+    //     .watch(PeerInfoRepository.provider)
+    //     .fetchPeerUserData(connectedPeers);
+
+    // final autosyncPeersData = await ref
+    //     .watch(PeerInfoRepository.provider)
+    //     .fetchPeerUserData(autosyncPeers);
 
     return SyncNotifyState(
-      connectedPeers: connectedPeersData,
-      autosyncPeers: autosyncPeers,
+      connectedPeers: [],
+      autosyncPeers: [],
+      patientInfo: ref.watch(patientInfoServiceProvider).patientInfoModel,
+      timeMetrics: ref.watch(timeMetricsRepositoryProvider).getTimeMetrics(),
     );
   }
 
-  void addAutosyncPeer(String deviceId) {
+  Future<void> addAutosyncPeer(String deviceId) async {
     ref.read(sharedPreferencesRepositoryProvider).addAutoSyncPeer(deviceId);
 
     final autosyncPeers =
         ref.watch(sharedPreferencesRepositoryProvider).getAutoSyncPeers();
 
+    final autosyncPeersData = await ref
+        .watch(PeerInfoRepository.provider)
+        .fetchPeerUserData(autosyncPeers);
+
     state = AsyncValue.data(
       SyncNotifyState(
         connectedPeers: state.value?.connectedPeers ?? [],
-        autosyncPeers: autosyncPeers,
+        autosyncPeers: autosyncPeersData,
       ),
     );
   }
 
-  void removeAutosyncPeer(String deviceId) {
+  Future<void> removeAutosyncPeer(String deviceId) async {
     ref.read(sharedPreferencesRepositoryProvider).removeAutoSyncPeer(deviceId);
 
     final autosyncPeers =
         ref.watch(sharedPreferencesRepositoryProvider).getAutoSyncPeers();
 
+    final autosyncPeersData = await ref
+        .watch(PeerInfoRepository.provider)
+        .fetchPeerUserData(autosyncPeers);
+
     state = AsyncValue.data(
       SyncNotifyState(
         connectedPeers: state.value?.connectedPeers ?? [],
-        autosyncPeers: autosyncPeers,
+        autosyncPeers: autosyncPeersData,
       ),
     );
   }
 
-  void onHandleSync(String deviceId, Map<String, dynamic> data) {
-    ref
-        .read(BridgefyService.provider)
-        .sendToDevice(deviceId: deviceId, data: data);
-    // Handle sync logic here
+  /// IMPORTANT: BridgefyService must be initialized and started before
+  /// calling this method.
+  Future<Result<String?, String>> onHandleSync(String? deviceId) async {
+    if (deviceId == null) {
+      return const Result.failure('Device ID is null');
+    }
+
+    final syncData = {
+      'patientInfo': _patientInfo.toMap(),
+      'timeMetrics': _timeMetrics.toMap(),
+    };
+
+    return Result.fromFuture(
+      ref
+          .read(BridgefyService.provider)
+          .sendToDevice(deviceId: deviceId, data: syncData),
+      onError: (error, stackTrace) {
+        return error.toString();
+      },
+    );
   }
+
+  PatientInfoService get _patentInfoRepository =>
+      ref.read(patientInfoServiceProvider);
+
+  PatientInfoModel get _patientInfo => _patentInfoRepository.patientInfoModel;
+
+  TimeMetricsRepository get _timeMetricsRepository =>
+      ref.read(timeMetricsRepositoryProvider);
+
+  TimeMetricsModel get _timeMetrics =>
+      _timeMetricsRepository.getTimeMetrics() ?? const TimeMetricsModel();
 }
 
 class SyncNotifyState extends Equatable {
   const SyncNotifyState({
     this.connectedPeers = const [],
     this.autosyncPeers = const [],
+    this.patientInfo,
+    this.timeMetrics,
   });
 
   final List<PeerUserData> connectedPeers;
-  final List<String> autosyncPeers;
+  final List<PeerUserData> autosyncPeers;
+  final PatientInfoModel? patientInfo;
+  final TimeMetricsModel? timeMetrics;
 
   @override
-  List<Object?> get props => [connectedPeers, autosyncPeers];
+  List<Object?> get props =>
+      [connectedPeers, autosyncPeers, patientInfo, timeMetrics];
 }
